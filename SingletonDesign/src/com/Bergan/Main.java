a4a7b6f1d389e4c0762cfa5026e5fb306a4af3a7
RamberganSelvanayagam <79039830+RamberganSelvanayagam@users.noreply.github.com>
2022-07-04 11:25:33 +0530

    Add files via upload
    singleton

---
+++ SingletonDesign/src/com/Bergan/Main.java
@@ -0,0 +1,10 @@
+package com.Bergan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Singleton singleton=Singleton.getInstance();
+        singleton.displayMessage();
+    }
+}
